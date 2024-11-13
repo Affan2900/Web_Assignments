2ee89dacf470da2474591fa9adc6b71bfe6e28e7
Affan2900 <affanamir290@gmail.com>
2024-11-13 22:08:20 +0500

    Task 1 only chnage background left

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
-function toggleDetails() {
-  const details = document.getElementById("details");
-  if (details.style.display === "none") {
-      details.style.display = "block";
+function toggleDetails(id) {
+  const details = document.querySelector(`.details-${id}`);
+  const button = document.querySelector(`button[onclick="toggleDetails(${id})"]`);
+  
+  if (details.style.display === "none" || !details.style.display) {
+    details.style.display = "block";
+    button.textContent = "Hide Details";
   } else {
-      details.style.display = "none";
+    details.style.display = "none";
+    button.textContent = "Show More Details";
   }
 }
